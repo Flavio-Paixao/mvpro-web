39a39084910ab18183f917a43957538db3fa4db5
FlavioMVP <flavio_paixao92@icloud.com>
2026-09-12 21:52:01 -0300

    Initial commit - MVPro Web (React + TypeScript + Tailwind v4)

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+﻿function App() {
+  return (
+    <div className="min-h-screen bg-bg text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-orange">MVPro funcionando!</h1>
+    </div>
+  )
+}
+
+export default App
